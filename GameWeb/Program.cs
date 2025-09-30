d9ea9a04e369cd84a4186c520bdb8a9c938b77b2
Oğuz <oguz.selvin@gmail.com>
2025-09-30 13:17:22 +0300

    Login,Register,ForgotPassword
    giriş,kullanıcı kayıt, şifremi unuttum kısmı controllerde oluşturuldu.
    
    Gelişim Aşamasında.

--- GameWeb/Program.cs
+++ GameWeb/Program.cs
@@ -1,6 +1,40 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(opt =>
+{
+    opt.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    opt.LoginPath = "/admin/login";
+    opt.LogoutPath = "/admin/logout";
+
+
+}
+
+);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseStatusCodePagesWithRedirects("/hata/{0}");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(name: "areas", pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+
+app.MapControllerRoute(name: "default", pattern: "{controller=home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
